cc47d197ddb5ae8144255e7087cc70d827cb7358
Pieter Van Puyenbroeck <pietervp283@gmail.com>
2017-11-19 15:57:21 +0100

    Extra uiviewcontrollers for registering user

--- MVCScore/Controller/LoginViewController.swift
+++ MVCScore/Controller/LoginViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var EmailText: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,7 +26,19 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func loginAuth(_ sender: Any) {
+        let alertFail = UIAlertController(title: "Authentication failed", message: "Wrong credentials", preferredStyle: .alert)
+        Auth.auth().signIn(withEmail: EmailText.text!, password: Password.text!) { (user, error) in
+            if error != nil {
+                self.present(alertFail, animated: true, completion: nil)
+            }else{
+                print("Login Succesfull")
+                SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "GoToApp", sender: self)
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
