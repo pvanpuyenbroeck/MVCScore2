b94585dd9ee803e90812f2620166327a6c9b24f3
Pieter Van Puyenbroeck <pietervp283@gmail.com>
2017-11-20 19:50:15 +0100

    add collection to firestorm

--- MVCScore/Controller/AppViewController.swift
+++ MVCScore/Controller/AppViewController.swift
@@ -9,28 +9,50 @@
 import UIKit
 import Firebase
 
-class AppViewController: UIViewController {
 
+class AppViewController: UIViewController {
+    
+    @IBOutlet weak var addCollectionButton: UIButton!
+    
+    let defaultStore = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addCollectionButton.layer.cornerRadius = 20
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func AddCollectionClicked(_ sender: Any) {
+        var ref: DocumentReference? = nil
+        ref = defaultStore.collection("users").addDocument(data: [
+            "first": "Pieter",
+            "last": "Van Puyenbroeck",
+            "born": "1987"]){
+                err in
+                if let err = err{
+                    print("Error adding document: \(err)")
+                    
+                }
+                else{
+                    print("Document added with ID: \(ref!.documentID)")
+                }
+        }
     }
-    */
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
