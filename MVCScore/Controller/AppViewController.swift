6ecc4d0ffc9f7b47057302414f8ba046d2ca5ef5
Pieter Van Puyenbroeck <pietervp283@gmail.com>
2017-12-02 14:27:16 +0100

    changes stuck

--- MVCScore/Controller/AppViewController.swift
+++ MVCScore/Controller/AppViewController.swift
@@ -14,53 +14,55 @@ class AppViewController: UIViewController, UITableViewDelegate,UITableViewDataSo
     @IBOutlet var TableViewTeams: UITableView!
     var teams : [Team] = [Team]()
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TeamCellTableViewCell", for: indexPath) as! TeamCellTableViewCell
-        
-        cell.TeamNameCell.text = "The White Russians"
-        
-        return cell
-    }
-    
-    
     
     let defaultStore = Firestore.firestore()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //Delegate instellen als self
         TableViewTeams.delegate = self
         TableViewTeams.dataSource = self
-        getMessages()
+        
         // Do any additional setup after loading the view.
         
         //TODO: Register your TableViewTeams file here:
-        TableViewTeams.register(UINib(nibName:"TeamCellTableViewCell", bundle:nil), forCellReuseIdentifier: "TeamCellTableViewCell")
+        TableViewTeams.register(UINib(nibName:"TeamCellTableViewCell", bundle:nil), forCellReuseIdentifier: "CustomTeamCellTableViewCell")
+        
+        getMessages()
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTeamCellTableViewCell", for: indexPath) as! TeamCellTableViewCell
+        
+        cell.TeamNameCell.text = teams[indexPath.row].TeamName
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     func getMessages(){
-        let teamRef = defaultStore.collection("Team")
-        
         defaultStore.collection("Team").getDocuments(){(querySnapshot, error) in
             if let error = error{
                 print(error)
             }
             else{
                 for document in querySnapshot!.documents{
+                    let team : Team = Team()
+                    team.TeamName = document.documentID
+                    self.teams.append(team)
                     print("\(document.documentID) => \(document.data())")
                 }
             }
+            
+            self.TableViewTeams.reloadData()
         }
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
     //    @IBAction func AddCollectionClicked(_ sender: Any) {
     //        var ref: DocumentReference? = nil
     //        ref = defaultStore.collection("users").addDocument(data: [
